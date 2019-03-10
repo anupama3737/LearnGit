62e14bc1ee463093a2d456292d04c4bf9fcfa451
Anupama <anu3737@gmail.com>
2019-03-10 17:30:37 +0530

    edit divid funtion

--- calc.py
+++ calc.py
@@ -1,11 +1,15 @@
 def add(x,y):
     (x+y)
     pass
+
 def subtract(x,y):
     pass
+
 def multiply(x,y):
     return x*y
+
 def divide(x,y):
-    pass
+    return x/y
+
 def square(x,y):
     pass
